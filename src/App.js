ea1aeba14ce1de79e68d2b9c804dc0e5fa7bb5fa
Shao Cherng Kong <skong@ukaachen.de>
2021-07-05 09:05:57 +0200

    Show Leberzirrhose

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import Box from './components/Box';
+import Leberzirrhose from './components/Leberzirrhose';
 
 function App() {
   return (
@@ -8,8 +8,7 @@ function App() {
     <ambientLight intensity={0.5} />
     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
     <pointLight position={[-10, -10, -10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
+    <Leberzirrhose position={[0, 0, 3]} />
   </Canvas>
   );
 }
