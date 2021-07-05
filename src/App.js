e876712f5ce14d7bc94cb0e733065b21d739a692
Shao Cherng Kong <skong@ukaachen.de>
2021-07-05 12:13:28 +0200

    Update Leberzirrhose with textures

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import Leberzirrhose from './components/Leberzirrhose';
-import Duck from './components/Duck'
 import { OrbitControls } from '@react-three/drei'
+import Leberzirrhose from './components/Leberzirrhose';
 
 function App() {
   return (
-    <Suspense fallback='null'>
+    <Suspense fallback='Loading'>
       <Canvas>
-      <OrbitControls />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Leberzirrhose position={[0, 0, 4]} />
-      <Duck position={[0, 0, 0]} />
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Leberzirrhose scale={[10, 10, 10]} position={[0, 0, 0]} />
       </Canvas>
     </Suspense>
   );
